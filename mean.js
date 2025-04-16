376c0c4180d7588f57d0b50a16c1e820104507b5
Guilherme662-prog <gui.lhermesilva1@live.com>
2025-04-16 12:21:49 -0300

    Update mean.js
    Edição pro jeito 16/04

--- mean.js
+++ mean.js
@@ -10,6 +10,15 @@ console.log(notas_allisson);
 
 console.log("AV1:  "+notas_allisson[0]);
 console.log("AV2:  "+notas_allisson[0]);
-notas_allisson[2] = (notas_allisson [0])
+notas_allisson[2] = (notas_allisson [notas allisson[2] = (notas allisson[0] + notas_allisson[1]) / 2;
+console.log(“Média: “notas_allisson[2]);
+console.log(“Todas: +notas_allisson);
 
 
+let allissson_nota_av1 = 8;
+let allisson _nota_av2 = 9.6;
+let allisson_media = (allisson_nota_av1 + allisson_nota_av2) / 2
+
+console.log(“Média Allisson “ + allisson_media);
+
+console.log(“Exemplo com arrays”)
